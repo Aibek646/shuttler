34ca621d027dfc4505650df971635cfbeba37070
Seanny Phoenix <seannyphoenix@gmail.com>
2020-04-09 19:11:16 -0700

    MOdels Port, Flight, Person

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Craft(models.Model):
+    pass
+
+
+# Seanny
+class Port(models.Model):
+    name = models.CharField(max_length=30)
+    abbr = models.CharField(max_length=4)
+
+# Seanny
+
+
+class Flight(models.Model):
+    craft = models.ForeignKey(Craft, on_delete=models.PROTECT)
+    departure_time = models.DateTimeField()
+    departure_port = models.ForeignKey(
+        Port, on_delete=models.PROTECT, related_name='departure')
+    arrival_time = models.DateTimeField()
+    arrival_port = models.ForeignKey(
+        Port, on_delete=models.PROTECT, related_name='arrival')
+
+# Seanny
+
+
+class Person(models.Model):
+    class Role(models.IntegerChoices):
+        STAFF = 1
+        CREW = 2
+        PASSENGER = 3
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=2, choices=(
+        ('ST', 'Staff'),
+        ('CR', 'Crew'),
+        ('PA', 'Passenger')
+    ))
+
+
+class Manifest(models.Model):
+    pass
